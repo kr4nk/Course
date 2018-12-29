f210759209b2aac6ed751f9196367f4dd9ad9813
Evgeny Kirichenko <e.kiri4enka@me.com>
2018-12-29 14:48:04 +0300

    added 1,2,3 task Enumerations

--- AlexSkutarenko_iOSDevCurse/Enumerations.playground/Contents.swift
+++ AlexSkutarenko_iOSDevCurse/Enumerations.playground/Contents.swift
@@ -3,54 +3,53 @@ import Cocoa
 var str = "Hello, Enumerations"
 
 //1. Создать энум с шахматными фигруами (король, ферзь и т.д.). Каждая фигура должна иметь цвет белый либо черный (надеюсь намек понят), а так же букву и цифру для позиции. Создайте пару фигур с расположением на доске, так, чтобы черному королю был мат :) Поместите эти фигуры в массив фигур
-
-enum ChessPiece {
-    case King(color:Color, position: (String, Int))
-    case Queen(color:Color, position: (String, Int))
-    case Rook(color:Color, position: (String, Int))
-    case Bishop(color:Color, position: (String, Int))
-    case Knight(color:Color, position: (String, Int))
-    case Pawn(color:Color, position: (String, Int))
-
+//2. Сделайте так, чтобы энумовские значения имели rawValue типа String. Каждому типу фигуры установите соответствующее английское название. Создайте функцию, которая выводит в консоль (текстово, без юникода) название фигуры, цвет и расположение. Используя эту функцию распечатайте все фигуры в массиве.
+enum Chess {
+    case Piece(name: ChessPiece, color: Color, col: String, row: Int)
+    
+    enum ChessPiece: String {
+        case King   = "King"
+        case Queen  = "Queen"
+        case Rook   = "Rook"
+        case Bishop = "Bishop"
+        case Knight = "Knight"
+        case Pawn   = "Pawn"
+    }
+        
     enum Color: String {
-        case black = "Black"
-        case white = "White"
+            case black = "Black"
+            case white = "White"
     }
 }
 
-var chessman1 = ChessPiece.King(color: .black, position: ("H",8))
-var chessman2 = ChessPiece.Rook(color: .black, position: ("E",5))
-var chessman3 = ChessPiece.King(color: .white, position: ("G",4))
-var chessman4 = ChessPiece.Rook(color: .white, position: ("C",7))
-var chessman5 = ChessPiece.Knight(color: .white, position: ("F",6))
+var chessman1 = Chess.Piece(name: .King, color: .black, col: "H", row: 8)
+var chessman2 = Chess.Piece(name: .Rook, color: .black, col: "E", row: 5)
+var chessman3 = Chess.Piece(name: .King, color: .white, col: "G", row: 4)
+var chessman4 = Chess.Piece(name: .Rook, color: .white, col: "C", row: 7)
+var chessman5 = Chess.Piece(name: .Knight, color: .white, col: "F", row: 6)
 
 var arabicMate = [chessman1, chessman2, chessman3, chessman4, chessman5]
 
-
-
-//2. Сделайте так, чтобы энумовские значения имели rawValue типа String. Каждому типу фигуры установите соответствующее английское название. Создайте функцию, которая выводит в консоль (текстово, без юникода) название фигуры, цвет и расположение. Используя эту функцию распечатайте все фигуры в массиве.
-
-enum PieceNames: String {
-    case King = "King"
-    case Queen = "Queen"
-    case Rook = "Rook"
-    case Bishop = "Bishop"
-    case Knight = "Knight"
-    case Pawn = "Pawn"
-}
-
-func printChessMove(chessMove: ChessPiece) {
+func printChessMove(chessMove: Chess) {
     switch chessMove {
-    case .King(color: let color, position: (let col, let row)): print("\(color) \(PieceNames.King), \(col, row)")
-    case .Rook(color: let color, position: (let col, let row)): print("\(color) \(PieceNames.Rook), \(col, row)")
-    case .Queen(color: let color, position: (let col, let row)): print("\(color) \(PieceNames.Queen), \(col, row)")
-    case .Bishop(color: let color, position: (let col, let row)): print("\(color) \(PieceNames.Bishop), \(col, row)")
-    case .Pawn(color: let color, position: (let col, let row)): print("\(color) \(PieceNames.Pawn), \(col, row)")
-    case .Knight(color: let color, position: (let col, let row)): print("\(color) \(PieceNames.Knight), \(col, row)")
+    case .Piece(let name, let color, let lett, let num):
+        print("\(color) \(name) - \(lett)\(num)")
+//    case .King(color: let color, position: (let col, let row)):
+//        print("\(color) \(PieceNames.King), \(col, row)")
+//    case .Rook(color: let color, position: (let col, let row)):
+//        print("\(color) \(PieceNames.Rook), \(col, row)")
+//    case .Queen(color: let color, position: (let col, let row)):
+//        print("\(color) \(PieceNames.Queen), \(col, row)")
+//    case .Bishop(color: let color, position: (let col, let row)):
+//        print("\(color) \(PieceNames.Bishop), \(col, row)")
+//    case .Pawn(color: let color, position: (let col, let row)):
+//        print("\(color) \(PieceNames.Pawn), \(col, row)")
+//    case .Knight(color: let color, position: (let col, let row)):
+//        print("\(color) \(PieceNames.Knight), \(col, row)")
     }
 }
 
-func arrayToPrint(array: [ChessPiece]) {
+func arrayToPrint(array: [Chess]) {
     for piece in array {
         printChessMove(chessMove: piece)
     }
@@ -59,6 +58,45 @@ func arrayToPrint(array: [ChessPiece]) {
 arrayToPrint(array: arabicMate)
 
 //3. Используя красивые юникодовые представления шахматных фигур, выведите в консоли вашу доску. Если клетка не содержит фигуры, то используйте белую или черную клетку. Это должна быть отдельная функция, которая распечатывает доску с фигурами (принимает массив фигур и ничего не возвращает)
+
+func unicodeBoard (piece: Chess) -> String {
+    let white = ["King": "♚", "Queen": "♛", "Bishop": "♝", "Knight":"♞", "Rook": "♜", "Pawn": "♟"]
+    let black = ["King": "♔", "Queen": "♕", "Bishop": "♗", "Knight":"♘", "Rook": "♖", "Pawn": "♙"]
+    
+    switch piece {
+    case .Piece(let name, let color, _, _):
+        return color == .white ? white[name.rawValue]! : black[name.rawValue]!
+    }
+}
+
+func printChessboard(chess: [Chess]) {
+    let col = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    var bCell = "\u{2b1b}"
+    var wCell = "\u{2b1c}"
+    for row in 1...8 {
+        var line = ""
+        line += "\(9 - row)"
+        for (index, column) in col.enumerated() {
+            var isEmpty = true
+            for piece in chess {
+                switch piece {
+                case .Piece(_, _, let l, let n) where l == column && n == (9 - row):
+                    line.append(unicodeBoard(piece: piece))
+                    isEmpty = false
+                default:
+                    break
+                }
+            }
+            if isEmpty {
+                (row + index) % 2 != 0 ? line.append(wCell) : line.append(bCell)
+            }
+        }
+        print(line)
+    }
+    print("  A B C D E F G H")
+}
+printChessboard(chess: arabicMate)
+    
 
 
 
