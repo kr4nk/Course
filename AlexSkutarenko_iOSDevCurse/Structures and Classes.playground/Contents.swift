aa50553e6ce7bf09261073844cda4b6005d24c29
Evgeny Kirichenko <e.kiri4enka@me.com>
2019-01-04 08:18:22 -0500

    added 6 task (Structures and Classes)

--- AlexSkutarenko_iOSDevCurse/Structures and Classes.playground/Contents.swift
+++ AlexSkutarenko_iOSDevCurse/Structures and Classes.playground/Contents.swift
@@ -42,7 +42,7 @@ rollCall(array: sortedArraybyScore)
 let student5 = Student(firstName: "Tomy", lastName: "Thompson", birthDate: 1992, averageScore: 2.5)
 let student6 = Student(firstName: "Bob", lastName: "Brown", birthDate: 1992, averageScore: 2.5)
 
-let journal2 = [student1, student2, student3, student4, student5, student6]
+let journal2 = journal + [student5, student6]
 
 var sortedArraybyName = journal2.sorted(by: {
     if $0.lastName == $1.lastName {
@@ -53,7 +53,6 @@ var sortedArraybyName = journal2.sorted(by: {
 print("\nSorted by Name")
 
 rollCall(array: sortedArraybyName)
-
 
 //5. Создайте переменную и присвойте ей ваш существующий массив. Измените в нем данные всех студентов. Изменится ли первый массив? Распечатайте оба массива.
 
@@ -72,6 +71,69 @@ print("\nNew journal")
 rollCall(array: journalNew)
 
 //6. Теперь проделайте все тоже самое, но не для структуры Студент, а для класса. Какой результат в 5м задании? Что изменилось и почему?
+class StudentClass {
+    let firstName: String
+    let lastName: String
+    let birthDate: Int
+    var averageScore: Double
+    
+    init(firstName: String, lastName: String, birthDate: Int, averageScore: Double) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.birthDate = birthDate
+        self.averageScore = averageScore
+    }
+}
+
+let studentCl1 = StudentClass(firstName: "Alex", lastName: "Holt", birthDate: 1995, averageScore: 4.0)
+let studentCl2 = StudentClass(firstName: "Mariam", lastName: "Johnson", birthDate: 1990, averageScore: 3.0)
+let studentCl3 = StudentClass(firstName: "Evie", lastName: "Brown", birthDate: 1997, averageScore: 5.0)
+let studentCl4 = StudentClass(firstName: "Samantha", lastName: "Thompson", birthDate: 1992, averageScore: 2.5)
+
+let journalCl = [studentCl1, studentCl2, studentCl3, studentCl4]
+
+func rollCallCl (array: [StudentClass]) {
+    var number = 0
+    for every in array {
+        number += 1
+        print("\(number): \(every.firstName) \(every.lastName), birth year: \(every.birthDate) and averege score:\(every.averageScore)")
+    }
+}
+
+rollCallCl(array: journalCl)
+
+var sortedArraybyScoreCl = journalCl.sorted(by: {$0.averageScore < $1.averageScore})
+print("\nSorted by Score")
+rollCallCl(array: sortedArraybyScoreCl)
+
+let studentCl5 = StudentClass(firstName: "Tomy", lastName: "Thompson", birthDate: 1992, averageScore: 2.5)
+let studentCl6 = StudentClass(firstName: "Bob", lastName: "Brown", birthDate: 1992, averageScore: 2.5)
+
+let journalCl2 = journalCl + [studentCl5, studentCl6]
+var sortedArraybyNameCl = journalCl2.sorted(by: {
+    if $0.lastName == $1.lastName {
+        return $0.firstName < $1.firstName
+    }
+    return $0.lastName < $1.lastName
+})
+print("\nSorted by Name")
+
+rollCallCl(array: sortedArraybyNameCl)
+
+
+var journalNewCl = journalCl2
+
+journalNewCl[0].averageScore = 3.0
+journalNewCl[1].averageScore = 3.1
+journalNewCl[2].averageScore = 3.2
+journalNewCl[3].averageScore = 3.3
+journalNewCl[4].averageScore = 3.4
+journalNewCl[5].averageScore = 3.5
+
+print("\nJournal 2")
+rollCallCl(array: journalCl2)
+print("\nNew journal")
+rollCallCl(array: journalNewCl)
 
 //007. Уровень супермен. Выполните задание шахмат из урока по энумам используя структуры либо классы
 
