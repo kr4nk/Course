5be11afbdfe116074e8b2eef279c5d53b8c79e98
Evgeny Kirichenko <e.kiri4enka@me.com>
2019-01-08 18:13:57 -0500

    few lines of code (Properties)

--- AlexSkutarenko_iOSDevCurse/Properties.playground/Contents.swift
+++ AlexSkutarenko_iOSDevCurse/Properties.playground/Contents.swift
@@ -65,4 +65,47 @@ student.fullName
 student.fullName = "Serg Bobov"
 student.fullName
 //===============================
+/*
+ 2. Добавить студенту property «Дата рождения» (пусть это будет еще одна структура, содержащая день, месяц, год) и два computed property: первое — вычисляющее его возраст, второе — вычисляющее, сколько лет он учился (считать, что он учился в школе с 6 лет, если студенту меньше 6 лет — возвращать 0)
+*/
 
+struct Student2 {
+    var firstName: String
+    var lastName: String
+    var fullName: String {
+        get {
+            return firstName + " " + lastName
+        }
+        set {
+            print("Full name wants to be set to " + newValue)
+            
+            let words = newValue.components(separatedBy: " ")
+            
+            if words.count > 0 {
+                firstName = words[0]
+            }
+            
+            if words.count > 1 {
+                lastName = words[1]
+            }
+        }
+    }
+    struct BirthDate {
+        var dayBirth: Int
+        var monthBirth: Int
+        var yearBirth: Int
+    }
+//   var age: Int {
+        
+   // }
+    
+    
+}
+
+
+print(Calendar.current.component(.day, from: Date()))
+print(Calendar.current.component(.month, from: Date()))
+print(Calendar.current.component(.year, from: Date()))
+print(Calendar.current.component(.hour, from: Date()))
+
+//Date.timeIntervalSince(Date())
