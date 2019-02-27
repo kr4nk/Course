5d43ebebc3716f3adf1d176afe2759d8168dbf4f
Evgeny Kirichenko <e.kiri4enka@me.com>
2019-02-27 10:18:18 +0300

    Course2Week3Task2 part 1

---
+++ OpenProfession_eLegion/Course2Week3Task2/Course2Week3Task2/Code/PhotoTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoTableViewCell.swift
+//  Course2Week3Task2
+//
+//  Created by Evgeny Kirichenko on 22/02/2019.
+//  Copyright © 2019 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: NSObject {
+
+}
